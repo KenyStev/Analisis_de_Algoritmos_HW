76ac8ef67853335e41b123fe23ed6842ca9bb20e
kenystev <kjestevez.0795@gmail.com>
2017-06-09 00:27:17 -0600

    KruskalMST is done

--- KruskalMST/main.cpp
+++ KruskalMST/main.cpp
@@ -5,14 +5,16 @@
 
 using namespace std;
 
-bool checkNode(int** graph,int* marks,int node,int size)
+bool checkNode(int** graph,int* marks,int father,int node,int size)
 {
+	bool value = false;
 	if (node < size)
 	{
 		marks[node] = 1;
+		cout<<"father: "<<father<<", node: "<<node<<endl;
 		for (int i = 0; i < size; ++i)
 		{
-			if (i!=node)
+			if (i!=node && i!=father)
 			{
 				if (graph[node][i] != -1)
 				{
@@ -20,50 +22,36 @@ bool checkNode(int** graph,int* marks,int node,int size)
 					{
 						return true;
 					}else{
-						return checkNode(graph,marks,i,size);
+						value |= checkNode(graph,marks,node,i,size);
 					}
 				}
 			}
 		}
 	}
-	return false;
+	return value;
 }
 
 bool hasCycle(int** graph, int size)
 {
 	int* marks = new int[size];
 	memset(marks,0,size*sizeof(int));
-	// return checkNode(graph,marks,0,size);
-
-	if (!checkNode(graph,marks,0,size))
-	{
-		for (int i = 0; i < size; ++i)
-		{
-			if (marks[i]==0)
-			{
-				cout<<"marks["<<i<<"] = "<<marks[i]<<endl;
-				if (checkNode(graph,marks,i,size))
-				{
-					return true;
-				}
-			}
-		}
-	}else{
-		return true;
-	}
-	return false;
+	return checkNode(graph,marks,0,0,size);
 }
 
 //-------------------------------------------------------------
 
 void insertEdge(int** graph,int size,Edge* edge)
 {
+	cout<<"added: graph["<<edge->source<<"]["<<edge->destination<<"]="<<edge->weight<<endl;
 	graph[edge->source][edge->destination] = edge->weight;
+	graph[edge->destination][edge->source] = edge->weight;
 }
 
 void removeEdge(int** graph,int size,Edge* edge)
 {
-	graph[edge->source][edge->destination] = -1;	
+	cout<<"has cicle, "<<"removed: graph["<<edge->source<<"]["<<edge->destination<<"]="<<edge->weight<<endl;
+	graph[edge->source][edge->destination] = -1;
+	graph[edge->destination][edge->source] = -1;
 }
 
 //-------------------------------------------------------------
@@ -71,19 +59,17 @@ void removeEdge(int** graph,int size,Edge* edge)
 int** getKruskalMST(int** graph, int size, vector<Edge*> edges)
 {
 	cout<<"getKruskalMST"<<endl;
-	// int** newGraph = new int*[size];
-	// for(int i=0; i<size; i++){
-	// 	newGraph[i] = new int[size];
-	// 	memset(newGraph[i],-1,size*sizeof(int));
-	// }
-	int** newGraph = graph;
+	int** newGraph = new int*[size];
+	for(int i=0; i<size; i++){
+		newGraph[i] = new int[size];
+		memset(newGraph[i],-1,size*sizeof(int));
+	}
+	// int** newGraph = graph;
     for (int i = 0; i < edges.size(); ++i)
     {
-    	if(newGraph[edges[i]->source][edges[i]->destination]==-1)
-    		insertEdge(newGraph,size,edges[i]);
+    	insertEdge(newGraph,size,edges[i]);
     	if (hasCycle(newGraph,size))
     	{
-    		cout<<"has cicle"<<endl;
     		removeEdge(newGraph,size,edges[i]);
     	}
     }
@@ -92,7 +78,8 @@ int** getKruskalMST(int** graph, int size, vector<Edge*> edges)
     {
     	for (int j = 0; j < size; ++j)
     	{
-    		cout<<"newGraph["<<i<<"]["<<j<<"] = "<<newGraph[i][j]<<endl;
+    		if(newGraph[i][j]!=-1)
+    			cout<<"newGraph["<<i<<"]["<<j<<"]="<<newGraph[i][j]<<";"<<endl;
     	}
     }
     return newGraph;
